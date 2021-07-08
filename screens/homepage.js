1df0c4c3567a8ea83bbd964542991f90f8842d07
James Pearson <james.pearson-cic-uk@ibm.com>
2021-07-08 09:16:15 +0100

    Adding card swiper for job vacancies

--- screens/homepage.js
+++ screens/homepage.js
@@ -1,23 +1,132 @@
-import React, { Component } from "react";
-import { StyleSheet, Button, View, Text } from "react-native";
+import React from "react";
+import { Image, StyleSheet, Text, View, SafeAreaView } from "react-native";
 
-class Home extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Home screen</Text>
+import data from "../mock/data";
+import Swiper from "react-native-deck-swiper";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const colors = {
+  red: "#EC2379",
+  blue: "#0070FF",
+  gray: "#777777",
+  white: "#ffffff",
+  black: "#000000",
+};
+
+const swiperRef = React.createRef();
+
+const Card = ({ card }) => {
+  return (
+    <View style={styles.card}>
+      <Image source={{ uri: card.image }} style={styles.cardImage} />
+    </View>
+  );
+};
+
+const CardDetails = ({ index }) => (
+  <View key={data[index].id} style={{ alignItems: "center" }}>
+    <Text style={[styles.text, styles.jobTitle]}>
+      Job Title - {data[index].jobTitle}
+    </Text>
+    <Text style={[styles.text, styles.salary]}>
+      Salary - {data[index].salary}
+    </Text>
+  </View>
+);
+
+export default function App() {
+  const [index, setIndex] = React.useState(0);
+  const onSwiped = () => {
+    setIndex((index + 1) % data.length);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.swiperContainer}>
+        <Swiper
+          ref={swiperRef}
+          cards={data}
+          cardIndex={index}
+          renderCard={(card) => <Card card={card} />}
+          backgroundColor={"transparent"}
+          onSwiped={onSwiped}
+          onTapCard={() => swiperRef.current.swipeLeft()}
+          cardVerticalMargin={50}
+        />
       </View>
-    );
-  }
+      <View style={styles.bottomContainer}>
+        <CardDetails index={index} />
+        <View style={styles.bottomContainerButtons}>
+          <MaterialCommunityIcons.Button
+            name="close"
+            size={94}
+            backgroundColor="transparent"
+            underlayColor="transparent"
+            activeOpacity={0.3}
+            color={colors.red}
+            onPress={() => swiperRef.current.swipeLeft()}
+          />
+          <MaterialCommunityIcons.Button
+            name="circle-outline"
+            size={94}
+            backgroundColor="transparent"
+            underlayColor="transparent"
+            activeOpacity={0.3}
+            color={colors.blue}
+            onPress={() => swiperRef.current.swipeRight()}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: colors.white,
   },
+  swiperContainer: {
+    flex: 0.55,
+  },
+  bottomContainer: {
+    flex: 0.45,
+    justifyContent: "space-evenly",
+  },
+  bottomContainerMeta: { alignContent: "flex-end", alignItems: "center" },
+  bottomContainerButtons: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  cardImage: {
+    width: 160,
+    flex: 1,
+    resizeMode: "contain",
+  },
+  card: {
+    flex: 0.45,
+    borderRadius: 8,
+    shadowRadius: 25,
+    shadowColor: colors.black,
+    shadowOpacity: 0.08,
+    shadowOffset: { width: 0, height: 0 },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.white,
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent",
+  },
+  done: {
+    textAlign: "center",
+    fontSize: 30,
+    color: colors.white,
+    backgroundColor: "transparent",
+  },
+  text: { fontFamily: "Courier" },
+  heading: { fontSize: 24, marginBottom: 10, color: colors.gray },
+  jobTitle: { color: colors.blue, fontSize: 20, fontWeight: "500" },
+  salary: { color: colors.blue, fontSize: 20, fontWeight: "500" },
 });
-
-export default Home;
